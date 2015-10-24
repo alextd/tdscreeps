f39775fafb1b000383c92d36a06fd3e3aaf038c7
Alex TD (Mac) <alextearsedoyle@gmail.com>
2015-10-24 03:33:32 -0700

    Split to modules

---
+++ code/builder.js
@@ -0,0 +1,15 @@
+module.exports = function (creep) {
+	if(creep.carry.energy == 0) {
+		if(Game.spawns.Spawn1.transferEnergy(creep) == ERR_NOT_IN_RANGE) {
+			creep.moveTo(Game.spawns.Spawn1);
+		}
+	}
+	else {
+		var sites = creep.room.find(FIND_CONSTRUCTION_SITES);
+		if(sites.length) {
+			if(creep.build(sites[0]) == ERR_NOT_IN_RANGE) {
+				creep.moveTo(sites[0]);
+			}
+		}
+	}
+}
--- code/main.js
+++ code/main.js
@@ -1,4 +1,6 @@
 var harvester = require('harvester');
+var builder = require('builder');
+var guard = require('guard');
 
 module.exports.loop = function () {
 
@@ -8,30 +10,11 @@ module.exports.loop = function () {
 		if(creep.memory.role == 'harvester') {
 			harvester(creep);
 		}
-
-		if(creep.memory.role == 'builder') {
-		
-			if(creep.carry.energy == 0) {
-				if(Game.spawns.Spawn1.transferEnergy(creep) == ERR_NOT_IN_RANGE) {
-					creep.moveTo(Game.spawns.Spawn1);				
-				}
-			}
-			else {
-				var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
-				if(targets.length) {
-					if(creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
-						creep.moveTo(targets[0]);					
-					}
-				}
-			}
+		else if(creep.memory.role == 'builder') {
+			builder(creep);
 		}
-		if(creep.memory.role == 'guard') {
-        	var targets = creep.room.find(FIND_HOSTILE_CREEPS);
-        	if(targets.length) {
-        		if(creep.attack(targets[0]) == ERR_NOT_IN_RANGE) {
-        			creep.moveTo(targets[0]);		
-				}
-			}
+		else if(creep.memory.role == 'guard') {
+			guard(creep);
 		}
 	}
 }
