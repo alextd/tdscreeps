d1eeb297ad8a62c1d88afde957c07bdc721bcf25
Alex TD (Mac) <alextearsedoyle@gmail.com>
2015-10-25 05:58:31 -0700

    Spawn selects source and assigns worker a job

--- code/main.js
+++ code/main.js
@@ -1,13 +1,38 @@
+var worker = require('worker');
 var harvester = require('harvester');
+var carrier = require('carrier');
 var builder = require('builder');
 var guard = require('guard');
 
+
+Spawn.prototype.selectSource = /** @return Source */ function() {
+	var source = this.pos.findClosestByRange(FIND_SOURCES);
+	this.memory.nextSourceId = source.id;
+	return source;
+};
+
 module.exports.loop = function () {
+	var /** @type Room */  room = Game.rooms[Object.keys(Game.rooms)[0]];
+	var /** @type Spawn */ spawn = room.find(FIND_MY_SPAWNS)[0];
+
+	var targetSource = spawn.selectSource();
+
+	var workerParts = [MOVE,MOVE,WORK,CARRY,CARRY];
+	if(spawn.spawning == null && spawn.canCreateCreep(workerParts) == OK)
+		spawn.createCreep(workerParts,
+			{"role" : "worker", "job": "source", jobid:targetSource.id, "homeid":spawn.id});
+
 
 	for(var name in Game.creeps) {
 		var creep = Game.creeps[name];
 
-		if(creep.memory.role == 'harvester') {
+		if(creep.memory.role == 'worker') {
+			worker(creep);
+		}
+		else if(creep.memory.role == 'carrier') {
+			carrier(creep);
+		}
+		else if(creep.memory.role == 'harvester') {
 			harvester(creep);
 		}
 		else if(creep.memory.role == 'builder') {
---
+++ code/worker.js
@@ -0,0 +1,19 @@
+module.exports = function (creep) {
+	switch(creep.memory.job)
+	{
+		case "source":
+			if(creep.carry.energy < creep.carryCapacity) {
+				var source = Game.getObjectById(creep.memory.jobid);
+				if(creep.harvest(source) == ERR_NOT_IN_RANGE) {
+					creep.moveTo(source);
+				}
+			}
+			else {
+				var spawn = Game.getObjectById(creep.memory.homeid);
+				if(creep.transferEnergy(spawn) == ERR_NOT_IN_RANGE) {
+					creep.moveTo(spawn);
+				}
+			}
+	}
+
+}
